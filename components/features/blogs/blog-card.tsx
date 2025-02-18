38970f474dc4b519da6a7ccaa57e8e7e349ddabe
onechie <angeloparole23@gmail.com>
2025-02-18 18:22:58 +0800

    Removed dark color on hover

--- components/features/blogs/blog-card.tsx
+++ components/features/blogs/blog-card.tsx
@@ -13,15 +13,13 @@ export default function BlogCard(props: BlogCard) {
   return (
     <Link
       href={`/blog/${id}`}
-      className="group border rounded h-80 flex-1 flex flex-col justify-between bg-white hover:scale-105 transition duration-200 hover:shadow-md hover:cursor-pointer hover:bg-gray-800"
+      className="border rounded h-80 flex-1 flex flex-col justify-between bg-white hover:scale-105 transition duration-200 hover:shadow-lg hover:cursor-pointer"
     >
-      <h1 className="truncate mx-5 py-2 border-b text-gray-800 group-hover:text-gray-100">
-        {title}
-      </h1>
-      <p className="truncate text-wrap flex-1 mx-5 my-2 text-gray-600 text-sm group-hover:text-gray-300">
+      <h1 className="truncate mx-5 py-2 border-b text-gray-800 ">{title}</h1>
+      <p className="truncate text-wrap flex-1 mx-5 my-2 text-gray-600 text-sm ">
         {content}
       </p>
-      <p className="truncate mx-5 py-3 border-t text-xs text-gray-800 group-hover:text-gray-100">
+      <p className="truncate mx-5 py-3 border-t text-xs text-gray-800 ">
         {author}
       </p>
     </Link>
